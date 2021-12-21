3813be5359eb5fa687c84e86c921d92dd977e7cd
saad.shafiq <saad.shafiq@systemsltd.com>
2021-12-21 15:25:28 +0500

    new change

--- src/app/configuration.ts
+++ src/app/configuration.ts
@@ -1,9 +1,9 @@
 export const configuration = {
   header: {
-    heading: 'Lift My Mood',
+    heading: 'Lift My ',
     headingtext:
       'This tool will anaylyse your mood according to text provided',
-    buttontext: 'Mood saad',
+    buttontext: 'Mood Lifter',
     buttonlink: '\Services'
   }
 };
